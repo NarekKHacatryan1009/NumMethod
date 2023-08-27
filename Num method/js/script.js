927701c9acbfb90f7b53aace49277ba989827dbd
NarekKHacatryan1009 <132485862+NarekKHacatryan1009@users.noreply.github.com>
2023-08-27 16:09:11 +0400

    NumMethod
    NumMethod

---
+++ Num method/js/script.js
@@ -0,0 +1,26 @@
+// .Math
+// let num = 2.505666
+// console.log(num);
+// console.log(num.toFixed(2)*100);
+
+// console.log(Math.floor(num)); //depi nerqev
+
+// console.log(Math.ceil(num));  //depi verev
+
+// console.log(Math.round(num)); //matematikakan kloracum
+
+// console.log(Math.random(num)*5);
+
+// console.log(Math.floor(Math.random(num)*5));
+
+// let num = "12.45px"
+// console.log(parseInt(num)); // 12
+
+// let num = "12.45px"
+// console.log(parseFloat(num)); // 12.45
+
+// let num = "12.45px"
+// let n = 12.45
+// console.log(parseFloat(num));
+// console.log(Math.trunc(n)); // 12
+// console.log(Math.pow(n,2));
